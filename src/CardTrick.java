ffced42bfdcfe1bb891ac06937ade63a4aca6c6e
karan <karan@DESKTOP-BLLP6N8.phub.net.cable.rogers.com>
2023-05-27 20:58:06 -0400

    Branch2

--- src/CardTrick.java
+++ src/CardTrick.java
@@ -3,30 +3,62 @@ import card.Card;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package card;
+
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author srinivsi
+ * 
+ * @author Karanveer Singh
+ * @since 27 May 2023
  */
 public class CardTrick {
-    
-    public static void main(String[] args)
-    {
-        Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+
+    public static void main(String[] args) {
+        Card[] magicHand = generateMagicHand(7);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the value of the card: ");
+        int value = scanner.nextInt();
+        System.out.print("Enter the suit of the card (0-3): ");
+        int suit = scanner.nextInt();
+
+        Card playersCard = new Card();
+        playersCard.setValue(value);
+        playersCard.setSuit(Card.SUITS[suit]);
+
+        boolean found = searchCard(magicHand, playersCard);
+
+        if (found) {
+            System.out.println("The card is present");
+        } else {
+            System.out.println("The card is not present");
         }
-        
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
     }
     
+    private static Card[] generateMagicHand(int size) {
+        Card[] magicHand = new Card[size];
+        Random random = new Random();
+
+        for (int i = 0; i < magicHand.length; i++) {
+            Card c = new Card();
+            c.setValue(random.nextInt(13) + 1);
+            c.setSuit(Card.SUITS[random.nextInt(4)]);
+            magicHand[i] = c;
+        }
+
+        return magicHand;
+    }
+
+    private static boolean searchCard(Card[] magicHand, Card card) {
+        for (Card c : magicHand) {
+            if (c.getValue() == card.getValue() && c.getSuit().equals(card.getSuit())) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- src/card/Card.java
+++ src/card/Card.java
@@ -11,7 +11,8 @@ package card;
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
- * @author srinivsi
+ * @author Karanveer Singh
+ * @since 27 May 2023
  */
 public class Card {
 
